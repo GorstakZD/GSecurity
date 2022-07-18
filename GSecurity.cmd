a349389aa23348f5f824567cfe0ac2a3ffdb8e74
Goran Štambuk <98338194+GorstakZD@users.noreply.github.com>
2022-07-19 00:31:10 +0200

    Add files via upload

--- GSecurity.cmd
+++ GSecurity.cmd
@@ -107,5 +107,19 @@ Reg.exe import Security.reg
 >nul 2>&1 DISM /Online /Enable-Feature /All /Quiet /NoRestart /FeatureName:Microsoft-Hyper-V
 >nul 2>&1 DISM /Online /Enable-Feature /All /Quiet /NoRestart /FeatureName:Containers-DisposableClientVM
 
+:: Hosts
+IF NOT EXIST HOSTS GOTO noHostsFile
+IF "%OS%"=="Windows_NT" GOTO HostsFile
+IF EXIST %winbootdir%\HOSTS*.* ATTRIB +A -H -R -S %winbootdir%\HOSTS*.*>NUL
+IF EXIST %winbootdir%\HOSTS.MVP DEL %winbootdir%\HOSTS.MVP>NUL
+IF EXIST %winbootdir%\HOSTS REN %winbootdir%\HOSTS HOSTS.MVP>NUL
+IF EXIST %winbootdir%\NUL COPY /Y HOSTS %winbootdir%>NUL
+GOTO noHostsFile
+:HostsFile
+IF EXIST %windir%\SYSTEM32\DRIVERS\ETC\HOSTS*.* ATTRIB +A -H -R -S %windir%\SYSTEM32\DRIVERS\ETC\HOSTS*.*>NUL
+IF EXIST %windir%\SYSTEM32\DRIVERS\ETC\HOSTS.MVP DEL %windir%\SYSTEM32\DRIVERS\ETC\HOSTS.MVP>NUL
+IF EXIST %windir%\SYSTEM32\DRIVERS\ETC\HOSTS REN %windir%\SYSTEM32\DRIVERS\ETC\HOSTS HOSTS.MVP>NUL
+IF EXIST %windir%\SYSTEM32\DRIVERS\ETC\NUL COPY /Y HOSTS %windir%\SYSTEM32\DRIVERS\ETC>NUL
+:noHostsFile
 ::Exit
 exit
